e1cb610146204c4870f399c4d6241ea07cc1b958
chrisbrady510 <chrismbrady@gmail.com>
2019-06-05 09:29:21 -0700

    first commit

---
+++ hangman.js
@@ -0,0 +1,235 @@
+1// Word selection
+// New word = ["Word name", "Hint"]
+var word = [["Hangman", "The game you are playing right now."], 
+["Christopher", "About the creator of this game."], 
+["Winter", "From Decemeber 21 through March 21"], 
+["Degree", "What you hope to earn in college"], 
+["Mario", "Iconic 90's video game character, still relevant today"], 
+["Pizza", "Favorite food of many people young and old"], 
+["Samsung", "Major tech company which produces many android phones"], 
+["San Jose", "Most populous city in Northern California and unofficial capital of Silicon Valley"], 
+["Sleep", "Something you do while in bed"], 
+["Clock", "Way to tell time"], 
+["Sunny", "Often synoymous with peoples connotations of California"], 
+["Hockey", "National sport of Canada that's gaining popularity in US"],
+["Eagle", "Offical bird of the United States"], 
+["Transporation", "How you get to work school or home is your mode of this"], 
+["Consent", "Another word for agree"]
+["Baseball", "America's unoffical pastime"]]
+
+// Game keyboard
+var tastatur = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+// Game memory
+var select = 0
+var wordLeft = []
+var fail = 0
+
+// Web-page onload
+window.onload = function() {
+    gId("moveKeybord").addEventListener('touchmove', function(e) {
+        wH = window.innerHeight
+        tY = e.touches[0].clientY
+        eL = gId("tastatur")
+        resY = wH - tY - eL.offsetHeight
+        if(resY < 0) {
+            resY = 0
+        } else if(resY > wH / 2) {
+            resY = wH / 2
+        }
+        eL.style.bottom = resY + "px"
+    }, false)
+    createTastur()
+}
+
+// Start game
+function startGame() {
+    gId("home").className = "h"
+    gId("result").className = "h"
+    newGame()
+}
+
+// New game
+function newGame() {
+    clearTastatur()
+    clearPlayer()
+    createWord()
+}
+
+// Clear keyboard
+function clearTastatur() {
+    var e = document.getElementsByClassName("b")
+    for(a = 0; a < e.length; a++) {
+        e[a].setAttribute("data", "")
+    }
+}
+
+// Clear player
+function clearPlayer() {
+    fail = 0
+    wordLeft = []
+    gId("g0").setAttribute("data", "false")
+    gId("g1").setAttribute("data", "false")
+    gId("g2").setAttribute("data", "false")
+    gId("g3").setAttribute("data", "false")
+    gId("g4").setAttribute("data", "false")
+    gId("g5").setAttribute("data", "false")
+    gId("g5").setAttribute("r", "false")
+    gId("g5").setAttribute("l", "false")
+    gId("g6").setAttribute("data", "false")
+    gId("g6").setAttribute("l", "false")
+    gId("g6").setAttribute("r", "false")
+    gId("hintButton").setAttribute("data", "false")
+    gId("hint").style.display = "none"
+}
+
+// Get new word
+function createWord() {
+    var d = gId("letter")
+    d.innerHTML = ""
+    select = Math.floor(Math.random() * word.length)
+    for(a = 0; a < word[select][0].length; a++) {
+        var x = word[select][0][a].toUpperCase()
+        var b = document.createElement("span")
+        b.className = "l" + (x == " " ? " ls" : "")
+        b.innerHTML = "&nbsp"
+        b.id = "l" + a;
+        d.appendChild(b)
+        
+        if(x != " ") {
+            if(wordLeft.indexOf(x) == -1) {
+                wordLeft.push(x)
+            }
+        }
+    }
+}
+
+// Create keyboard
+function createTastur() {
+    var tas = gId("keybord")
+    tas.innerHTML = ""
+    for(a = 0; a < tastatur.length; a++) {
+        var b = document.createElement("span")
+        b.className = "b"
+        b.innerText = tastatur[a]
+        b.setAttribute("data", "")
+        b.onclick = function() {
+            bTas(this)
+        }
+        tas.appendChild(b)
+    }
+}
+
+// Game check, If show next error / game end
+function bTas(a) {
+    if(a.getAttribute("data") == "") {
+        var x = isExist(a.innerText)
+        a.setAttribute("data", x)
+        if(x) {
+            if(wordLeft.length == 0) {
+                gameEnd(true)
+            }
+        } else {
+            showNextFail()
+        }
+    }
+}
+
+// If letter "X" exist
+function isExist(e) {
+    e = e.toUpperCase()
+    var x = wordLeft.indexOf(e)
+    if(x != -1) {
+        wordLeft.splice(x, 1)
+        typeWord(e)
+        return true
+    }
+    return false
+}
+
+// Show next fail drawing
+function showNextFail() {
+    fail++
+    switch(fail) {
+        case 1:
+            gId("g0").setAttribute("data", "true")
+            break;
+        
+        case 2:
+            gId("g1").setAttribute("data", "true")
+            break;
+        
+        case 3:
+            gId("g2").setAttribute("data", "true")
+            break;
+        
+        case 4:
+            gId("g3").setAttribute("data", "true")
+            gId("hintButton").setAttribute("data", "true")
+            break;
+        
+        case 5:
+            gId("g4").setAttribute("data", "true")
+            break;
+        
+        case 6:
+            gId("g5").setAttribute("data", "true")
+            break;
+        
+        case 7:
+            gId("g5").setAttribute("l", "true")
+            break;
+        
+        case 8:
+            gId("g5").setAttribute("r", "true")
+            break;
+        
+        case 9:
+            gId("g6").setAttribute("data", "true")
+            gId("g6").setAttribute("l", "true")
+            break;
+        
+        case 10:
+            gId("g6").setAttribute("r", "true")
+            gameEnd(false)
+            break;
+    }
+}
+
+function typeWord(e) {
+    for(a = 0; a < word[select][0].length; a++) {
+        if(word[select][0][a].toUpperCase() == e) {
+            gId("l" + a).innerText = e
+        }
+    }
+}
+
+// Game result
+function gameEnd(e) {
+    var d = gId("result")
+    d.setAttribute("data", e)
+    if(e) {
+        gId("rT").innerText = "You Win!"
+        gId("rM").innerHTML = "Congratulations, you solved the word!<br/><br/>You're amazing!"
+    } else {
+        gId("rT").innerText = "Sorry, you Lose! Better luck next time!"
+        gId("rM").innerHTML = "The word was <br/><br/>\"" + word[select][0].toUpperCase() + "\"<br/><br/>Don't give up, practice makes perfect."
+    }
+    d.className = ""
+}
+
+// Show hint
+function hint() {
+    gId("hintText").innerText = word[select][1]
+    gId("hint").style.display = "block"
+}
+
+// Exit hint
+function hintExit() {
+    gId("hint").style.display = "none"
+}
+
+// Get HTML ID element by name
+function gId(a) {
+    return document.getElementById(a)
+}
